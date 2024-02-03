0f4abc08d3c5d3176e03732e2549ad6ad2c6ac8f
gracebir <grace.devolop@gmail.com>
2024-02-03 16:12:24 +0200

    buttom bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,36 @@
+import 'package:fl_shopping_cart/components/bottom_navbar.dart';
+import 'package:fl_shopping_cart/pages/cart_page.dart';
+import 'package:fl_shopping_cart/pages/shop_page.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  //this selected index is to contrl he bottom nav bar
+  int _selectedIndex = 0;
+
+  // this method will update the our selected index
+  void navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  final List<Widget> _pages = [const ShopPage(), const CartPage()];
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      bottomNavigationBar: MyBottomNavBar(
+        onTabChange: (index) => navigateBottomBar(index),
+      ),
+      body: _pages[_selectedIndex],
+    );
   }
 }
