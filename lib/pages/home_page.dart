2a9c510a016d8f4041c3f39944e60fa3b10499dd
gracebir <grace.devolop@gmail.com>
2024-02-04 06:07:28 +0200

    drawer

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -30,6 +30,66 @@ class _HomePageState extends State<HomePage> {
       bottomNavigationBar: MyBottomNavBar(
         onTabChange: (index) => navigateBottomBar(index),
       ),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Builder(builder: (context) {
+          return IconButton(
+            icon: const Padding(
+              padding: EdgeInsets.only(left: 12.0),
+              child: Icon(
+                Icons.menu,
+                color: Colors.black,
+              ),
+            ),
+            onPressed: () {
+              Scaffold.of(context).openDrawer();
+            },
+          );
+        }),
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.black,
+        child: Column(
+          children: [
+            DrawerHeader(
+                child: Image.asset(
+              'lib/images/nike.png',
+              color: Colors.white,
+            )),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Divider(color: Colors.grey[800]),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 25),
+              child: ListTile(
+                leading: Icon(
+                  Icons.home,
+                  color: Colors.white,
+                ),
+                title: Text(
+                  "Home",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 25),
+              child: ListTile(
+                leading: Icon(
+                  Icons.info,
+                  color: Colors.white,
+                ),
+                title: Text(
+                  "Ahout",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
       body: _pages[_selectedIndex],
     );
   }
