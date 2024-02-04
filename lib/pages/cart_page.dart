0fa6039261a05c9f7345b98f36160217f900ad0a
gracebir <grace.devolop@gmail.com>
2024-02-04 10:09:08 +0200

    cart page

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,12 +1,41 @@
+import 'package:fl_shopping_cart/components/cart_item.dart';
+import 'package:fl_shopping_cart/models/cart.dart';
+import 'package:fl_shopping_cart/models/shoe_model.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Cart page"),
-    );
+    return Consumer<Cart>(
+        builder: (context, value, child) => Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "My cart",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Expanded(
+                        child: ListView.builder(
+                            itemCount: value.getUserCart().length,
+                            itemBuilder: (context, index) {
+                              // get invidual shoes
+                              Shoe invidualShoe = value.getUserCart()[index];
+
+                              return CartItem(
+                                shoe: invidualShoe,
+                              );
+                              // return the cart item
+                            }))
+                  ]),
+            ));
   }
 }
--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -1,81 +1,100 @@
 import 'package:fl_shopping_cart/components/shoe_tile.dart';
+import 'package:fl_shopping_cart/models/cart.dart';
 import 'package:fl_shopping_cart/models/shoe_model.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class ShopPage extends StatelessWidget {
+class ShopPage extends StatefulWidget {
   const ShopPage({super.key});
 
   @override
+  State<ShopPage> createState() => _ShopPageState();
+}
+
+class _ShopPageState extends State<ShopPage> {
+  //
+
+  void addShoeToCart(Shoe shoe) {
+    Provider.of<Cart>(context, listen: false).addItemToCart(shoe);
+    showDialog(
+        context: context,
+        builder: (context) => const AlertDialog(
+              title: Text("SuccessFully added"),
+              content: Text("check your cart"),
+            ));
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          padding: EdgeInsets.all(12),
-          margin: EdgeInsets.symmetric(horizontal: 25),
-          decoration: BoxDecoration(
-              color: Colors.grey[100], borderRadius: BorderRadius.circular(8)),
-          child: const Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                "Search",
-                style: TextStyle(color: Colors.grey),
-              ),
-              Icon(
-                Icons.search,
-                color: Colors.grey,
-              )
-            ],
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 25),
-          child: Text(
-            "Everyone flies..., other longer that others",
-            style: TextStyle(color: Colors.grey[600]),
-          ),
-        ),
-        // hot picks
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 25),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Text(
-                "Hot Picks 🔥",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-              ),
-              Text(
-                "Sell All",
-                style:
-                    TextStyle(fontWeight: FontWeight.bold, color: Colors.blue),
-              )
-            ],
-          ),
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-        Expanded(
-            child: ListView.builder(
-                itemCount: 4,
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) {
-                  Shoe shoe = Shoe(
-                      name: "All Start",
-                      price: "240",
-                      imagePath: "lib/images/all_star.jpg",
-                      description: "cool shoe");
-                  return ShoeTile(
-                    shoe: shoe,
-                  );
-                })),
-        const Padding(
-          padding: EdgeInsets.only(top: 25, right: 25, left: 25),
-          child: Divider(color: Colors.white),
-        )
-      ],
-    );
+    return Consumer<Cart>(
+        builder: (context, value, child) => Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(12),
+                  margin: const EdgeInsets.symmetric(horizontal: 25),
+                  decoration: BoxDecoration(
+                      color: Colors.grey[100],
+                      borderRadius: BorderRadius.circular(8)),
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Search",
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                      Icon(
+                        Icons.search,
+                        color: Colors.grey,
+                      )
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 25),
+                  child: Text(
+                    "Everyone flies..., other longer that others",
+                    style: TextStyle(color: Colors.grey[600]),
+                  ),
+                ),
+                // hot picks
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        "Hot Picks 🔥",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 24),
+                      ),
+                      Text(
+                        "Sell All",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, color: Colors.blue),
+                      )
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Expanded(
+                    child: ListView.builder(
+                        itemCount: 4,
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (context, index) {
+                          Shoe shoe = value.getShoeList()[index];
+                          return ShoeTile(
+                            shoe: shoe,
+                            onTap: () => addShoeToCart(shoe),
+                          );
+                        })),
+                const Padding(
+                  padding: EdgeInsets.only(top: 25, right: 25, left: 25),
+                  child: Divider(color: Colors.white),
+                )
+              ],
+            ));
   }
 }
